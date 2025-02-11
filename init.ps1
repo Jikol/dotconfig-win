39a14f15cc6cb52c99900f576ba4351a64199b53
Ondřej Javorský <gikol113@gmail.com>
2025-02-11 12:03:35 +0100

    feat: add btop config outside .config

--- init.ps1
+++ init.ps1
@@ -1,4 +1,4 @@
-# Create symbolic links
+# Helper functions
 function ln {
     param (
         [string]$Link,
@@ -8,9 +8,20 @@ function ln {
     New-Item -ItemType SymbolicLink -Path $Link -Target $Target | Out-Null
 }
 
+## Symlink program config outside ~/.config directory ##
+
+# SSH
 ln "$HOME\.ssh" "$HOME\.config\.ssh"
+
+# WindowsTerminal
 ln "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$HOME\.config\wt\settings.json"
+
+# PowerShell
 ln "$HOME\Documents\PowerShell\profile.ps1" "$HOME\.config\pwsh\profile.ps1"
+
+# Btop
+ln "$HOME\scoop\apps\btop\current\btop.conf" "$HOME\.config\btop\btop.conf"
+ln "$HOME\scoop\apps\btop\current\themes" "$HOME\.config\btop\themes"
 
 Write-Host "Symbolic links created!" -ForegroundColor Green
 
